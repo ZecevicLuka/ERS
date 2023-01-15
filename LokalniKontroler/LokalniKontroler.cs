0c6c5f30a16cc96099d7f9a3028dd32ae26d618e
knezevicc <72078503+knezevicc@users.noreply.github.com>
2023-01-15 17:12:01 +0100

    Add files via upload
    napr proj

---
+++ LokalniKontroler/LokalniKontroler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LokalniKontroler
+{
+    public enum Vrednost { open, close, on, off, analogMeasurement }
+
+    public class LokalniKontroler
+    {
+        public string ImeLokalnogKontrolera { get; set; }
+
+        //ovde nije int lupila sam
+        public int TimeStamp { get; set; }
+
+        public Vrednost ActualValue { get; set; }
+
+
+        public LokalniKontroler() { }
+
+        public LokalniKontroler(string imeLokalnogKontrolera, int timeStamp, Vrednost actualValue)
+        {
+            if (ImeLokalnogKontrolera == null )
+            {
+                throw new ArgumentNullException("Lokalni kontroler mora da ima ime");
+            }
+
+            if (ImeLokalnogKontrolera.Trim() == "")
+            {
+                throw new ArgumentException("Ime mora da sadrzi karaktere");
+            }
+
+
+            ImeLokalnogKontrolera = imeLokalnogKontrolera;
+            TimeStamp = timeStamp;
+            //nmp ovo
+            //ActualValue = Vrednost(actualValue);
+        }
+    }
+}
